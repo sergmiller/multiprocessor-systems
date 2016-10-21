34131a74acc13e811148095cb87772f0241ff09d
sergmiller <sergey.miller@phystech.edu>
2016-10-21 22:16:09 +0300

    add mpi calc pi

---
+++ MPI OpenMP Test/piwithmpi.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <mpi.h>
+#include <assert.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main(int argc, char** argv) {
+        long long  n = atoll(argv[1]);
+        int th, rank;
+ 	MPI_Status status;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&th);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+        int left = rank*(n/th), right = (rank + 1)*(n/th);
+        if(rank + 1 == th) {
+		right += n % th;
+	}
+	long  double pi = 0;
+        for(long long i = left;i < right;++i) {
+                pi += pow(-1,((2*i+1)%4 == 3))/(2*i + 1);
+        }
+
+	if(rank) {
+		MPI_Send(&pi, 1, MPI_LONG_DOUBLE, 0, 13, MPI_COMM_WORLD);
+	}
+
+	long double next_part;
+	if(!rank) {
+		for(long long i = 1;i < th;++i) { 
+                	MPI_Recv(&next_part, 1, MPI_LONG_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+        		pi += next_part;
+		}
+	}
+
+	MPI_Finalize();
+	if(!rank) {
+        	pi *= 4;
+		cout.precision(20);
+        	cout << "pi ~ " << pi << endl;
+	}
+        return 0;
+}
--- MPI OpenMP Test/sumwithomp.cpp
+++ MPI OpenMP Test/sumwithomp.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <stdio.h>
 #include <omp.h>
+#include <math.h>
 #include <assert.h>
 #include <iostream>
 #include <vector>
@@ -11,35 +11,36 @@ using std::endl;
 using std::vector;
 
 int main(int argc, char* argv[]) {
-	int n = atoi(argv[1]);
-	int th = atoi(argv[2]);
-	
-	vector <int> v(n);
-	double start, end;
-	
-	srand(time(NULL));
-	
-	for(int i = 0;i < n;++i) {
-		v[i] = rand();
-	} 
-	
-	start = omp_get_wtime();
-	double check_sum = 0;
-	for(int i = 0;i < n;++i) {
-		check_sum += sin(v[i]);
-	}
-	end = omp_get_wtime();
-	cout << "series time: " << (int)(end - start) << " s, n = " << n << ", th =" << th << endl;
-	start = omp_get_wtime();
-	double cur_sum = 0;
-	#pragma omp parallel for reduction (+: cur_sum) num_threads(th)
-	for(int i = 0;i < n;++i) {
+        int n = atoi(argv[1]);
+        int th = atoi(argv[2]);
+
+        vector <int> v(n);
+        double start, end;
+
+        srand(time(NULL));
+
+        for(int i = 0;i < n;++i) {
+                v[i] = rand();
+        }
+
+        start = omp_get_wtime();
+        double check_sum = 0;
+        for(int i = 0;i < n;++i) {
+                check_sum += sin(v[i]);
+        }
+        end = omp_get_wtime();
+        cout << "series time: " << (int)(end - start) << " s, n = " << n << ", th =" << th << endl;
+        start = omp_get_wtime();
+        double cur_sum = 0;
+        #pragma omp parallel for reduction (+: cur_sum) num_threads(th)
+        for(int i = 0;i < n;++i) {
                 cur_sum += sin(v[i]);
         }
-	#pragma end parallel
-	assert(abs(cur_sum - check_sum) < 1e-18);
-	end = omp_get_wtime();
-	cout << "OpenMP time: " << (int)(end - start) << " s, n = " << n << ", th =" << th << endl
-;   
-	return 0;
+        #pragma end parallel
+        assert(abs(cur_sum - check_sum) < 1e-18);
+        end = omp_get_wtime();
+        cout << "OpenMP time: " << (int)(end - start) << " s, n = " << n << ", th =" << th << endl
+;
+        return 0;
 }
+
